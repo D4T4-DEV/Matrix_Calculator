ce82a51a2b351919fb4938e5a882c727ab32be48
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-09-08 12:43:34 -0600

    Se cambia el props y se utiliza en un componente

--- src/Components/input_component.tsx
+++ src/Components/input_component.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { InputProps } from "../Interfaces/Props/inputProp";
 
 
-const Input: React.FC<InputProps> = ({ className, type, placeholder, value, min, max, disabled, onChange }) => {
+const Input: React.FC<InputProps> = ({ title, className, type, placeholder, value, min, max, disabled, onChange }) => {
     return (
         <input
+            title={title}
             className={className}
             type={type}
             placeholder={placeholder}
--- src/Interfaces/Props/inputProp.ts
+++ src/Interfaces/Props/inputProp.ts
@@ -1,6 +1,7 @@
 import { ChangeEvent } from "react";
 
 export interface InputProps {
+    title?: string;
     className?: string;
     type: string;
     placeholder?: string;
