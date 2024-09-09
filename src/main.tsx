e3473ed07f5248b0e37414053cdeb9b9b8f1b161
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-09-08 22:00:37 -0600

    Creacion e importacion de estilos
    Cabe mencionar que se intento hacer que este "estético", pero no soy demasiado diestro en este ámbito

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MatrizForm } from "./Components/form-matrix";
 
+import './Styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
