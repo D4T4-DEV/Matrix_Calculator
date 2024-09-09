e3bd238fe4d57cdaee62bebd04824e024277c92a
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2024-09-09 12:37:41 -0600

    Cambios para poder limitar el input
    Cambiamos la limitante en el ingreso de los números posibles por el índice de la matriz, para que no se vea feo. Si quieres que se vea continuo, deberás considerar quitar ese medio

--- src/Components/personalizateMatrix.tsx
+++ src/Components/personalizateMatrix.tsx
@@ -5,6 +5,10 @@ import { MatrixProps } from "../Interfaces/Props/MatrizProp";
 
 const MatrixComponent: React.FC<MatrixProps> = ({ matrix, onMatrixChange }) => {
 
+  // Variables que delimitan el numero posible
+  const limitNegative: number = -100;
+  const limitPositive: number = 100;
+
   // Verificacion de los indices existentes de una matriz y actualicacion de los datos dentro de esta
   // Si no hay esto no se cambia los valores 
   const handleChange = (row: number, col: number, value: string) => {
@@ -13,6 +17,18 @@ const MatrixComponent: React.FC<MatrixProps> = ({ matrix, onMatrixChange }) => {
     );
     onMatrixChange(updatedMatrix);
   };
+
+  // Funcion para limitar el numero que debe contener por teclado el input
+  const limitValue = (e: ChangeEvent<HTMLInputElement>) => {
+    // Si no tiene nada lo devuelve como nada (esto evita que se muestre el cero)
+    if (e.target.value === '') {
+      return '';
+    }
+
+    // Utilidad para limitar el valor posible del input
+    const limitNum = (Math.max(limitNegative, Math.min(limitPositive, Number(e.target.value))));
+    return limitNum.toString(); // Devolvemos en formato string
+  }
 
   return (
     <div className="mat-Component">
@@ -25,7 +41,9 @@ const MatrixComponent: React.FC<MatrixProps> = ({ matrix, onMatrixChange }) => {
               type="number"
               placeholder={`${rowIndex + 1}, ${colIndex + 1}`}
               value={value}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(rowIndex, colIndex, e.target.value)}
+              max={limitPositive}
+              min={limitNegative}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(rowIndex, colIndex, limitValue(e))}
             />
           ))}
         </div>
