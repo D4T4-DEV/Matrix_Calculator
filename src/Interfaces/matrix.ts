3d895f60e39160e0a28f01717baeb5991c0940a8
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-09-08 22:34:56 -0600

    Explicación de error
    Se explica si existe la incertidumbre de porque se usa un medio de dos dimensiones y porque no se es igual al comentario de las interfaces.

--- src/Interfaces/matrix.ts
+++ src/Interfaces/matrix.ts
@@ -6,15 +6,41 @@ export interface Matrix {
     matriz: number[][];
 }
 
-// Lo de arriba equivale a esto: 
-// export interface Matrix1D {
-//     dimension: number[];
-// };
 
-// export interface Matrix2D {
-//     dimension: number[][];
-// }
+// CORRECCION: 
+// LO QUE MUESTRO ACONTINUACION (ABAJO DE ESTA EXPLICACION👇) ES ERRONEO, DEBIDO A QUE EN UNA MATRIZ 
+// SE COMPONE DE FILAS Y COLUMNAS, POR LO QUE UN ARRAY BIDIMENSIONAL CUENTA CON LA 
+// CAPACIDAD DE PODER OPERAR EN MATRICES DE DIFERENTES DIMENSIONES, CONFUNSION MIA, AL ASIGNARLE 
+// LA COMPOSICION DE UN EJE.
 
-// export interface Matrix3D {
-//     matrix: number[][][];
-// }
+// LAS MATRICES SE COMPONEN EN FILAS Y COLUMNAS, PODRIAMOS DECIR QUE SON (X,Y)
+// X-> VALOR DE LAS COLUMNAS 
+// Y-> VALOR DE LAS FILAS 
+
+// POR LO QUE PARA REPRESENTAR UNA MATRIZ SOLO HACE FALTA UN ARRAY BIDIMENSIONAL 
+    // Lo de arriba equivale a esto (NO HACER CASO, LEER ARRIBA =/): 
+
+
+// ASPECTO ERRONEO 👇
+    // export interface Matrix1D { -> Es un arreglo unidimensional solo tiene filas (n) -> o la letra que prefieras 😃
+    //     dimension: number[];
+    // };
+
+    // export interface Matrix2D {
+    //     dimension: number[][]; -> Medio correcto para operar con matrices (filas, columnas) (x,y)
+    // }
+
+    // export interface Matrix3D { -> Medio incorrecto, ya que tiene filas, columnas, profundidad (x,y,z)
+    //     matrix: number[][][];
+    // }
+
+// EXPLICACION: ANTES CREE UNA INTERFAZ QUE SE LE PODIA PONDER LO QUE RECIBA como la que se ve abajo
+// donde por DIMENSION se iba a usar para asignar un array de numeros (o lo que quisieras xd) que correspondia a lo que decia arriba 
+// pero ciertamente PARA TRABAJAR CON MATRICES SE UTILIZA UN ARREGLO DE 2x2 (filas y columnas)
+/*
+    export interface MiERROR<DIMENSION> {
+        row: number;
+        col: number;
+        matriz: DIMENSION;
+    }
+*/
